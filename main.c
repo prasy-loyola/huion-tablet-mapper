429d00fc280ceb18bc291a2fcb56497394e9c19e
Prasanna Selvaraj <prasy.loyola@gmail.com>
2023-12-13 02:42:56 +0800

    cleanup

--- main.c
+++ main.c
@@ -1,93 +1,98 @@
 #include "raylib.h"
+#include <assert.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
+struct AppState {
+  bool appResized;
+  bool mappingAttempted;
+  bool mappingSuccessful;
+};
+struct AppState APPSTATE = {};
 
-  Color bg_color = DARKGRAY;
-  const static char *TABLET_STYLUS_NAME = "HUION H420 Pen stylus";
-  const static char *TABLET_NAME = "HUION H420 Pad pad";
-  const static char *MAP_KEY = "xsetwacom --set \"%s\" Button %d \"key %s\"";
+int mapButton(const char *tabletName, int button, char *key) {
+  char command[1000] = {};
+  sprintf(command, "xsetwacom --set \"%s\" Button %d \"key %s\"", tabletName,
+          button, key);
+  return system(command);
+}
+
+int mapTabletArea(const char *tabletName) {
+  int monitor_count = GetMonitorCount();
+  int screen_width = 0;
+  int screen_height = 0;
+  for (int i = 0; i < monitor_count; ++i) {
+    Vector2 monitor_pos = GetMonitorPosition(i);
+    if (screen_width < GetMonitorWidth(i) + monitor_pos.x) {
+      screen_width = GetMonitorWidth(i) + monitor_pos.x;
+    }
+    if (screen_height < GetMonitorHeight(i) + monitor_pos.y) {
+      screen_height = GetMonitorHeight(i) + monitor_pos.y;
+    }
+  }
+
+  int curr_height = GetRenderHeight();
+  int curr_width = GetRenderWidth();
+  Vector2 window_pos = GetWindowPosition();
+  // c0 = touch_area_width / total_width
+  float c0 = (float)curr_width / screen_width;
+  // c2 = touch_area_height / total_height
+  float c2 = (float)curr_height / screen_height;
+  // c1 = touch_area_x_offset / total_width
+  float c1 = window_pos.x / screen_width;
+  // c3 = touch_area_y_offset / total_height
+  float c3 = window_pos.y / screen_height;
+  char command[1000] = {};
   const static char *UPDATE_COORD_TRANS_MATRIX =
       "xinput set-prop \"%s\" "
       "--type=float "
       "\"Coordinate Transformation Matrix\" "
       "%f 0 %f 0 %f %f 0 0 1";
+  sprintf(command, UPDATE_COORD_TRANS_MATRIX, tabletName, c0, c1, c2, c3);
+  printf("%s\n", command);
+  return system(command);
+}
+
+int main(int argc, char **argv) {
+
+  Color bg_color = DARKGRAY;
+  const static char *TABLET_STYLUS_NAME = "HUION H420 Pen stylus";
+  const static char *TABLET_NAME = "HUION H420 Pad pad";
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(60);
   InitWindow(800, 450, "Huion Tablet mapper");
-  char text[3000] = {0};
   char info[3000] = {0};
   bool mapped = false;
   int prev_mapped_width = GetRenderWidth();
   int prev_mapped_height = GetRenderHeight();
   while (!WindowShouldClose()) {
     if (IsWindowResized()) {
+      APPSTATE.appResized = true;
+      APPSTATE.mappingAttempted = false;
+      APPSTATE.mappingSuccessful = false;
       int curr_width = GetRenderWidth();
       int curr_height = GetRenderHeight();
       int new_height = floorf(curr_width * (2.23 / 4));
       prev_mapped_width = curr_width;
       prev_mapped_height = new_height;
       SetWindowSize(curr_width, new_height);
+    } else {
+      APPSTATE.appResized = false;
     }
     if (IsKeyDown(KEY_M)) {
-      int monitor_count = GetMonitorCount();
-      int screen_width = 0;
-      int screen_height = 0;
-      for (int i = 0; i < monitor_count; ++i) {
-        Vector2 monitor_pos = GetMonitorPosition(i);
-        if (screen_width < GetMonitorWidth(i) + monitor_pos.x) {
-          screen_width = GetMonitorWidth(i) + monitor_pos.x;
-        }
-        if (screen_height < GetMonitorHeight(i) + monitor_pos.y) {
-          screen_height = GetMonitorHeight(i) + monitor_pos.y;
-        }
-      }
-
-      int curr_height = GetRenderHeight();
-      int curr_width = GetRenderWidth();
-      Vector2 window_pos = GetWindowPosition();
-      // c0 = touch_area_width / total_width
-      float c0 = (float)curr_width / screen_width;
-      // c2 = touch_area_height / total_height
-      float c2 = (float)curr_height / screen_height;
-      // c1 = touch_area_x_offset / total_width
-      float c1 = window_pos.x / screen_width;
-      // c3 = touch_area_y_offset / total_height
-      float c3 = window_pos.y / screen_height;
-
-      sprintf(text, UPDATE_COORD_TRANS_MATRIX, TABLET_NAME, c0, c1, c2, c3);
-      printf("%s\n", text);
-      int result1 = system(text);
-      mapped = true;
-      if (result1 != 0) {
-        mapped = false;
-      }
-
-      sprintf(text, UPDATE_COORD_TRANS_MATRIX, TABLET_STYLUS_NAME, c0, c1, c2,
-              c3);
-      printf("%s\n", text);
-      int result2 = system(text);
-
-      if (result2 != 0 || result1 != 0) {
-        mapped = false;
-        sprintf(info, "ERROR: failed mapping tablet to area");
+      APPSTATE.mappingAttempted = true;
+      if (mapTabletArea(TABLET_NAME) || mapTabletArea(TABLET_STYLUS_NAME) ||
+          mapButton(TABLET_NAME, 1, "c") || mapButton(TABLET_NAME, 2, "k") ||
+          mapButton(TABLET_NAME, 3, "e") ||
+          mapButton(TABLET_STYLUS_NAME, 3, "z")) {
+        APPSTATE.mappingSuccessful = false;
       } else {
-        mapped = true;
+        APPSTATE.mappingSuccessful = true;
       }
-
-      sprintf(text, MAP_KEY, TABLET_NAME, 1, "c");
-      system(text);
-      sprintf(text, MAP_KEY, TABLET_NAME, 2, "k");
-      system(text);
-      sprintf(text, MAP_KEY, TABLET_NAME, 3, "e");
-      system(text);
-      sprintf(text, MAP_KEY, TABLET_STYLUS_NAME, 3, "z");
-      system(text);
     }
     BeginDrawing();
     ClearBackground(bg_color);
@@ -95,10 +100,12 @@ int main(int argc, char **argv) {
              "whichever area you want to map to the tablet\n"
              "Press 'M' key when you are happy with the area\n",
              10, 100, 20, LIGHTGRAY);
-    if (mapped) {
-      DrawText("Mapped to the area!! \n", 10, 190, 25, GREEN);
-    } else {
-      DrawText(info, 10, 190, 25, RED);
+    if (APPSTATE.mappingAttempted) {
+      if (APPSTATE.mappingSuccessful) {
+        DrawText("Mapped to the area!! \n", 10, 190, 25, GREEN);
+      } else {
+        DrawText("Mapping failed", 10, 190, 25, RED);
+      }
     }
     EndDrawing();
   }
